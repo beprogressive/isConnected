c8adaa88d7a076759190eeda7174b21d4f40e486
beprogressive <1769696+beprogressive@users.noreply.github.com>
2026-05-15 23:25:54 +0200

    Show network speed in megabytes per second

--- NetworkSpeedFormatter.cs
+++ NetworkSpeedFormatter.cs
@@ -2,27 +2,11 @@ namespace IsConnected;
 
 internal static class NetworkSpeedFormatter
 {
-    private const decimal Kilo = 1_000m;
-    private const decimal Mega = Kilo * Kilo;
-    private const decimal Giga = Mega * Kilo;
+    private const decimal BytesPerMegabyte = 1_000_000m;
 
     public static string FormatBitsPerSecond(long bitsPerSecond)
     {
-        if (bitsPerSecond < 1_000)
-        {
-            return "0 bit/s";
-        }
-
-        if (bitsPerSecond < 1_000_000)
-        {
-            return $"{Math.Round(bitsPerSecond / Kilo)} Kbit/s";
-        }
-
-        if (bitsPerSecond < 1_000_000_000)
-        {
-            return $"{bitsPerSecond / Mega:0.#} Mbit/s";
-        }
-
-        return $"{bitsPerSecond / Giga:0.#} Gbit/s";
+        var megabytesPerSecond = bitsPerSecond / 8m / BytesPerMegabyte;
+        return $"{megabytesPerSecond:0.##} MB/s";
     }
 }
